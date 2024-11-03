27ca2bfe5197449535ca8fcc3850e199d05127e0
Wallker <mohamedtarekhafez4@gmail.com>
2024-11-03 16:03:44 +0200

    new stuff

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,17 @@
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
 function App() {
     return (
         <div>
             <WelcomeMessage />
             {/* Other components or content can go here */}
+            <Header />
+            <MainContent />
+            <Footer />
         </div>
     );
 }
+
