840521721895a25af7fae24a0f69113a9fdc5726
Wallker <mohamedtarekhafez4@gmail.com>
2024-11-03 15:54:29 +0200

    new stuff

---
+++ src/src/App.jsx
@@ -0,0 +1,9 @@
+import WelcomeMessage from './components/WelcomeMessage';
+function App() {
+    return (
+        <div>
+            <WelcomeMessage />
+            {/* Other components or content can go here */}
+        </div>
+    );
+}
