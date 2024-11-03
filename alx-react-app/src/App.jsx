4df70a2a82453fe72479da725143ce28d76503f1
Wallker <mohamedtarekhafez4@gmail.com>
2024-11-03 16:08:03 +0200

    new stuff

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,8 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
 
 function App() {
     return (
@@ -10,6 +12,11 @@ function App() {
             {/* Other components or content can go here */}
             <Header />
             <MainContent />
+            <UserProfile 
+                name="Alice" 
+                age="25" 
+                bio="Loves hiking and photography" 
+            />
             <Footer />
         </div>
     );
