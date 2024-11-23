1382bfd9b4c07b449d1ad68ff335ed236d905966
Wallker <mohamedtarekhafez4@gmail.com>
2024-11-23 16:44:48 +0200

    new stuff

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,16 +1,10 @@
-import React from 'react';
-import RegistrationForm from './components/RegistrationForm';
-import FormikForm from './components/FormikForm';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const App = () => (
-  <div>
-    <h1>React Form Handling</h1>
-    <h2>Controlled Component Form</h2>
-    <RegistrationForm />
-    <h2>Formik Form</h2>
-    <FormikForm />
-  </div>
+return (
+  <QueryClientProvider client={queryClient}>
+    <PostsComponent />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
 
-export default App;
 
