8230a00cfb16249f9521de1fa01d873c0b9c0584
Wallker <mohamedtarekhafez4@gmail.com>
2024-11-23 16:58:06 +0200

    new stuff

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 
+// Fetch function for posts
 const fetchPosts = async () => {
   const response = await fetch('https://jsonplaceholder.typicode.com/posts');
   if (!response.ok) {
@@ -10,15 +11,31 @@ const fetchPosts = async () => {
 };
 
 const PostsComponent = () => {
-  const { data, error, isLoading, isError, refetch } = useQuery('posts', fetchPosts);
+  const { 
+    data, 
+    error, 
+    isLoading, 
+    isError, 
+    refetch, 
+    isFetching 
+  } = useQuery('posts', fetchPosts, {
+    // Cache settings
+    cacheTime: 1000 * 60 * 5, // Cache time in ms (5 minutes)
+    staleTime: 1000 * 30,     // Data is considered fresh for 30 seconds
+    refetchOnWindowFocus: false, // Disable refetch when window is focused
+    keepPreviousData: true,   // Keep old data when refetching
+  });
 
+  // Display loading or error messages
   if (isLoading) return <p>Loading...</p>;
   if (isError) return <p>Error: {error.message}</p>;
 
   return (
     <div>
       <h1>Posts</h1>
-      <button onClick={() => refetch()}>Refetch Posts</button>
+      <button onClick={() => refetch()} disabled={isFetching}>
+        {isFetching ? 'Refetching...' : 'Refetch Posts'}
+      </button>
       <ul>
         {data.map((post) => (
           <li key={post.id}>
@@ -32,3 +49,4 @@ const PostsComponent = () => {
 };
 
 export default PostsComponent;
+
