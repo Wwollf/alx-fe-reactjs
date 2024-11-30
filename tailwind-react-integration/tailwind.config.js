af965172a917786246503c9ffc44c60b9f33606c
Wallker <mohamedtarekhafez4@gmail.com>
2024-11-30 12:06:43 +0200

    new stuff

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Paths to your content files
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Specify files to scan for classes
   theme: {
-    extend: {}, // Extend default theme
+    extend: {}, // Extend default theme if needed
   },
-  plugins: [], // Add plugins here if needed
+  plugins: [], // Add plugins here if required
+  darkMode: "class", // Enables dark mode via a `.dark` class on a parent element
+
 };
 
 
