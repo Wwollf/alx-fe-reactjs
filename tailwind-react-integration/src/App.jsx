ca7d59ed3707f60287670d37ba4af01fa25155c5
Wallker <mohamedtarekhafez4@gmail.com>
2024-11-30 12:15:55 +0200

    new stuff

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import UserProfile from "./components/UserProfile";
 import './App.css'
 
 function App() {
@@ -35,6 +36,9 @@ function App() {
     </div>
     <div className="bg-gray-50 min-h-screen flex items-center justify-center">
       <h1 className="text-4xl font-bold text-blue-600">Tailwind CSS Setup Verified!</h1>
+    </div>
+    <div>
+      <UserProfile />
     </div>
     </>
   )
