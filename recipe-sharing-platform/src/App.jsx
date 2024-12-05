c304ef37911fea8fb65daa5b7b7883188ea861fd
Wallker <mohamedtarekhafez4@gmail.com>
2024-12-05 17:24:07 +0200

    new stuff

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Home from './pages/HomePage';
+import HomePage from './components/HomePage';
 import './App.css'
 
 function App() {
