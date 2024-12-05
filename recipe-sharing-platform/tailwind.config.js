a01876c6157d4d522618dce580056913153d42d1
Wallker <mohamedtarekhafez4@gmail.com>
2024-12-05 17:09:42 +0200

    new stuff

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // Change to 'media' or 'class' if you want to enable dark mode
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
